a8a46c49a4f0e24f49e506d197885e5a58cfd42c
Evgeny Pichugin <epichugin@openintegration.inc>
2022-12-23 20:21:00 +0300

    added RegistrationTests

--- src/test/java/pages/RegistrationPageBurgers.java
+++ src/test/java/pages/RegistrationPageBurgers.java
@@ -12,12 +12,33 @@ public class RegistrationPageBurgers {
 		this.driver = driver;
 	}
 
+	//Поле "Имя"
+	private By nameField = By.xpath("//fieldset[1]//input");
+
 	//Поле "Email"
-	private By emailField = By.xpath("//input[@type='text' and @name='name']");
+	private By emailField = By.xpath("//fieldset[2]//input");
+
+	//Поле "Пароль"
+	private By passwordField = By.xpath("//input[@type = 'password']");
+
+	//Область с текстом ошибки
+	private By errorLocator = By.xpath("//p[@class = 'input__error text_type_main-default']");
 
 	@Step("Click button by label {buttonText}")
 	public void clickButtonByText(String buttonText) {
 		driver.findElement(By.xpath(String.format("//*[text()='%s']", buttonText))).click();
 	}
 
+	@Step("Get error text")
+	public String getErrorText() {
+		return driver.findElement(errorLocator).getText();
+	}
+
+	@Step("User registration")
+	public void register(String name, String email, String password){
+		driver.findElement(nameField).sendKeys(name);
+		driver.findElement(emailField).sendKeys(email);
+		driver.findElement(passwordField).sendKeys(password);
+		clickButtonByText("Зарегистрироваться");
+	}
 }
--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,11 +1,65 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.*;
+
+import java.util.concurrent.TimeUnit;
+
 public class RegistrationTests {
 
-	//Регистрация
-	//Проверь:
-	//Успешную регистрацию.
-	//Ошибку для некорректного пароля. Минимальный пароль — шесть символов.
+	private WebDriver driver;
 
+	private HomePageBurgers homePage;
+	private LoginPageBurgers loginPage;
+	private RegistrationPageBurgers registrationPage;
 
+	private String name = System.currentTimeMillis() + "name";
+	private String email = System.currentTimeMillis() + "@ya.ru";
+	private String password = System.currentTimeMillis() + "password";
+
+	private static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.get(BASE_URL);
+		homePage = new HomePageBurgers(driver);
+		loginPage = new LoginPageBurgers(driver);
+		registrationPage = new RegistrationPageBurgers(driver);
+		homePage.clickButtonByText("Войти в аккаунт");
+		loginPage.clickButtonByText("Зарегистрироваться");
+	}
+
+	@Test
+	@DisplayName("User registration")
+	@Description("Basic test for positive user registration")
+	public void positiveUserRegistration() throws InterruptedException {
+		registrationPage.register(name, email, password);
+		Thread.sleep(1000);
+		loginPage.login(email, password);
+
+		Assert.assertTrue(homePage.isOrderButtonDisplayed());
+	}
+
+	@Test
+	@DisplayName("Failed user registration")
+	@Description("Check user is not registered when password has < 6 symbols")
+	public void negativeUserRegistration() {
+		registrationPage.register(name, email, "12345");
+
+		Assert.assertEquals("Некорректный пароль", registrationPage.getErrorText());
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
 }
